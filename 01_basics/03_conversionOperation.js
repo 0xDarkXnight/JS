79dcebb2c257ef99eae73d20d9b0acd86e9136d9
Akshat Goyal <akshat_g1@me.iitr.ac.in>
2025-03-12 19:35:04 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -24,5 +24,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// OPERATIONS
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true)
+console.log(+"")
+// both not preferred
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 // readability not good
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
