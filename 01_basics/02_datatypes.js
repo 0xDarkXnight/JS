0e705a1dfcea5d7168f57c2ad4ba40e7a004363e
Akshat Goyal <akshat_g1@me.iitr.ac.in>
2025-03-12 18:53:30 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Akshat"
+let age = 18
+let isLoggedIn = false
+
+console.log(name);
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (type is object)
+// undefined 
+// symbol => unique
+
+// object
+console.log(typeof null);
