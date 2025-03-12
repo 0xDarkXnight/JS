3a210b4fdb03d2922109c39de95771566a01a488
Akshat Goyal <akshat_g1@me.iitr.ac.in>
2025-03-12 18:53:22 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1443
+let accountEmail = "hue@hue.com"
+accountName = "hue" //aise bhi kar sakte variable ko declare
+//dont use var because of issue in block scope and functinal scope. 
+console.log(accountId);
+console.table([accountEmail, accountId, accountName]);
