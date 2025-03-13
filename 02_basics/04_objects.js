3940fe14b6f70240bf08b0df9747d559ea467447
Akshat Goyal <akshat_g1@me.iitr.ac.in>
2025-03-13 21:09:36 +0000

    Objects part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,54 @@
+// const tinderUser = new Object() // singleton object
+const tinderUser = {} // non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Berlin"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        userFullName: {
+            firstName: "Akshat",
+            lastName: "Goyal"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName); // context would be regularUser.fullname?.userFullName.firstName if I want to check that is there a fullname object inside my regularUser object and if yes then proceed.
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "b@gmail.com"
+    },
+    {
+        id:3,
+        email: "c@gmail.com"
+    }
+]
+
+console.log(users[1].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // output is in form of array
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
