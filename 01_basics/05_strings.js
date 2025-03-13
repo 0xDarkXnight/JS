6f93a14e2833d72283fb01e8ff31f88fe9fb0e2e
Akshat Goyal <akshat_g1@me.iitr.ac.in>
2025-03-13 09:52:40 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Akshat"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value")
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Billu-Saanda-dot-com")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('u'));
+
+const newString = gameName.substring(0, 5)
+console.log(newString);
+
+const anotherString = gameName.slice(-11,5)
+console.log(anotherString);
+
+const newStringOne = "    Akshat    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://akshat.com/akshat%20goyal"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('akshat'))
+console.log(url.includes('billu'))
+
+console.log(gameName.split('-'))
+console.log(gameName.split('-',2))
